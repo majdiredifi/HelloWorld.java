187bd90e8ca43bf7cae95418f9365739138e8b70
Majdi Redifi <majdi.redifi@gmail.com>
2022-10-04 00:17:25 +0200

    add

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
-public class HelloWorld {
+public class HelloMajdi {
 
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World, Majdi");
+        System.out.println("Hello, Majdi!!!!");
     }
 
 }
